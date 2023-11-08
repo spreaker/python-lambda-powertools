07bd2cebfb1c83202cfd6caaa5607a1aa054c9ec
Mauro Piccotti <mauro.piccotti@spreaker.com>
2023-11-08 17:32:02 +0100

    wip

--- lambda_powertools/prometheus.py
+++ lambda_powertools/prometheus.py
@@ -1,40 +1,63 @@
+import os
 import json
-import prometheus_client as client
-from lambda_powertools.metric_encoder import MetricEncoder
+
+os.environ['PROMETHEUS_DISABLE_CREATED_SERIES'] = 'True'
+import prometheus_client
+from prometheus_client import CollectorRegistry
+
+prometheus_client.REGISTRY.unregister(prometheus_client.GC_COLLECTOR)
+prometheus_client.REGISTRY.unregister(prometheus_client.PLATFORM_COLLECTOR)
+prometheus_client.REGISTRY.unregister(prometheus_client.PROCESS_COLLECTOR)
 
 
 def reset():
-    collectors = list(client.REGISTRY._collector_to_names.keys())
-    for collector in collectors:
-        client.REGISTRY.unregister(collector)
+    for collector in list(prometheus_client.REGISTRY._collector_to_names.keys()):
+        if "_value" in dir(collector):
+            # case Counter without labels
+            collector._value.set(0)
+        elif "_metrics" in dir(collector):
+            for metric in collector._metrics.values():
+                if "_value" in dir(metric):
+                    # case Counter with labels
+                    metric._value.set(0)
+                elif "_buckets" in dir(metric) and "_sum" in dir(metric):
+                    # case Histogram with labels
+                    metric._sum.set(0)
+                    for bucket in metric._buckets:
+                        bucket.set(0)
+        elif "_buckets" in dir(collector) and "_sum" in dir(collector):
+            # case Histogram without labels
+            collector._sum.set(0)
+            for bucket in collector._buckets:
+                bucket.set(0)
 
 
 def filter_metrics(m):
     # Other metric types are not supported so far
-    if not m.type == "counter" and not m.type == "histogram":
+    if not m._type == "counter" and not m._type == "histogram":
         return False
 
     # Empty metrics with labels are exported without values
-    if len(m.samples) == 0:
+    dir_m = dir(m)
+    if "_value" in dir_m and m._value._value == 0:
         return False
-
-    # Empty metrics without labels are exported with zero value and empty labels ¯\_(ツ)_/¯
-    if len(m.samples) == 1 and m.samples[0].value == 0.0 and len(m.samples[0].labels.keys()) == 0:
+    if "_metrics" in dir_m and len(m._metrics.values()) == 0:
+        return False
+    if "_buckets" in dir_m and m._sum._value == 0:
         return False
 
     return True
 
 
 def get_metrics():
-    data = client.REGISTRY.collect()
+    new_registry = CollectorRegistry(auto_describe=True)
+    for collector in list(prometheus_client.REGISTRY._collector_to_names.keys()):
+        if (filter_metrics(collector)):
+            new_registry.register(collector)
 
-    # Remove _created metrics (hack because PROMETHEUS_DISABLE_CREATED_SERIES is not working)
-    for metric in data:
-        metric.samples = [sample for sample in metric.samples if not sample.name.endswith("_created")]
+    data = prometheus_client.generate_latest(new_registry).decode("utf-8")
 
-    metrics = list(filter(filter_metrics, data))
-
-    return metrics
+    return data
 
 
 def flush_metrics():
@@ -43,6 +66,4 @@ def flush_metrics():
     if not metrics or len(metrics) == 0:
         return
 
-    json_metrics = json.dumps(metrics, cls=MetricEncoder)
-
-    print(f'PROMLOG {json_metrics}')
+    print("PROMLOG [" + json.dumps(metrics) + "]")
--- tests/test_prometheus.py
+++ tests/test_prometheus.py
@@ -1,87 +1,62 @@
 import pytest
-import os
-import prometheus_client as client
-from unittest import mock
-from prometheus_client import Counter, Histogram, Gauge, Metric
-from prometheus_client.samples import Sample
 from lambda_powertools.prometheus import get_metrics, reset
+from prometheus_client import Counter, Histogram, Gauge
 
-counter_no_labels = None
-counter_with_labels = None
-histogram_no_labels = None
-histogram_with_labels = None
-gauge = None
+counter_no_labels = Counter(
+    name="prometheus_spec_counter_no_labels",
+    documentation="Prometheus example counter without labels"
+)
+
+counter_with_labels = Counter(
+    name="prometheus_spec_counter_with_labels",
+    documentation="Prometheus example counter with labels",
+    labelnames=["foo"]
+)
+
+histogram_no_labels = Histogram(
+    name="prometheus_spec_histogram_no_labels",
+    documentation="Prometheus example histogram without labels",
+    buckets=[1, 2, 5]
+)
+
+histogram_with_labels = Histogram(
+    name="prometheus_spec_histogram_with_labels",
+    documentation="Prometheus example histogram with labels",
+    buckets=[1, 2, 5],
+    labelnames=["foo"]
+)
+
+gauge = Gauge(
+    name="prometheus_spec_gauge_metric_name",
+    documentation="gauge_metric_documentation"
+)
 
 
 @pytest.fixture(autouse=True)
-@mock.patch.dict(os.environ, {"PROMETHEUS_DISABLE_CREATED_SERIES": "True"})
 def before_each(monkeypatch):
-    # Trying to disable default collector metrics, not under test
-    try:
-        client.REGISTRY.unregister(client.GC_COLLECTOR)
-        client.REGISTRY.unregister(client.PLATFORM_COLLECTOR)
-        client.REGISTRY.unregister(client.PROCESS_COLLECTOR)
-    except:
-        pass
-
-    global counter_no_labels, counter_with_labels, histogram_no_labels, histogram_with_labels, gauge
-
-    counter_no_labels = Counter(
-        name="prometheus_spec_counter_no_labels",
-        documentation="Prometheus example counter without labels"
-    )
-
-    counter_with_labels = Counter(
-        name="prometheus_spec_counter_with_labels",
-        documentation="Prometheus example counter with labels",
-        labelnames=["foo"]
-    )
-
-    histogram_no_labels = Histogram(
-        name="prometheus_spec_histogram_no_labels",
-        documentation="Prometheus example histogram without labels",
-        buckets=[1, 2, 5]
-    )
-
-    histogram_with_labels = Histogram(
-        name="prometheus_spec_histogram_with_labels",
-        documentation="Prometheus example histogram with labels",
-        buckets=[1, 2, 5],
-        labelnames=["foo"]
-    )
-
-    gauge = Gauge(
-        name="prometheus_spec_gauge_metric_name",
-        documentation="gauge_metric_documentation"
-    )
+    reset()
 
 
-# def test_prometheus_get_metrics_does_not_return_empty_metrics():
-#     reset()
-#
-#     metrics = get_metrics()
-#
-#     assert metrics == []
+def test_prometheus_get_metrics_does_not_return_empty_metrics():
+    reset()
+
+    metrics = get_metrics()
+
+    assert metrics == ""
 
 
-# def test_prometheus_get_metrics_returns_non_empty_metrics():
-#     counter_no_labels.inc(1)
-#     counter_with_labels.labels("bar").inc(2)
-#     histogram_no_labels.observe(1)
-#     histogram_with_labels.labels("bar").observe(1)
-#     gauge.set(1)  # Gauge is not supported yet
-#
-#     metrics = get_metrics()
-#
-#     expected = [
-#         Metric("prometheus_spec_counter_no_labels", "Prometheus example counter without labels", "counter", ""),
-#         Metric("prometheus_spec_counter_with_labels", "Prometheus example counter with labels", "counter", ""),
-#         Metric("prometheus_spec_histogram_no_labels", "Prometheus example histogram without labels", "histogram", ""),
-#         Metric("prometheus_spec_histogram_with_labels", "Prometheus example histogram with labels", "histogram", "")
-#     ]
-#     expected[0].samples = [Sample(name='prometheus_spec_counter_no_labels_total', labels={}, value=1.0, timestamp=None, exemplar=None)]
-#     expected[1].samples = [Sample(name='prometheus_spec_counter_with_labels_total', labels={'foo': 'bar'}, value=2.0, timestamp=None, exemplar=None)]
-#     expected[2].samples = [Sample(name='prometheus_spec_histogram_no_labels_bucket', labels={'le': '1.0'}, value=1.0, timestamp=None, exemplar=None), Sample(name='prometheus_spec_histogram_no_labels_bucket', labels={'le': '2.0'}, value=1.0, timestamp=None, exemplar=None), Sample(name='prometheus_spec_histogram_no_labels_bucket', labels={'le': '5.0'}, value=1.0, timestamp=None, exemplar=None), Sample(name='prometheus_spec_histogram_no_labels_bucket', labels={'le': '+Inf'}, value=1.0, timestamp=None, exemplar=None), Sample(name='prometheus_spec_histogram_no_labels_count', labels={}, value=1.0, timestamp=None, exemplar=None), Sample(name='prometheus_spec_histogram_no_labels_sum', labels={}, value=1.0, timestamp=None, exemplar=None)]
-#     expected[3].samples = [Sample(name='prometheus_spec_histogram_with_labels_bucket', labels={'foo': 'bar', 'le': '1.0'}, value=1.0, timestamp=None, exemplar=None), Sample(name='prometheus_spec_histogram_with_labels_bucket', labels={'foo': 'bar', 'le': '2.0'}, value=1.0, timestamp=None, exemplar=None), Sample(name='prometheus_spec_histogram_with_labels_bucket', labels={'foo': 'bar', 'le': '5.0'}, value=1.0, timestamp=None, exemplar=None), Sample(name='prometheus_spec_histogram_with_labels_bucket', labels={'foo': 'bar', 'le': '+Inf'}, value=1.0, timestamp=None, exemplar=None), Sample(name='prometheus_spec_histogram_with_labels_count', labels={'foo': 'bar'}, value=1.0, timestamp=None, exemplar=None), Sample(name='prometheus_spec_histogram_with_labels_sum', labels={'foo': 'bar'}, value=1.0, timestamp=None, exemplar=None)]
-#
-#     assert metrics == expected
+def test_prometheus_get_metrics_returns_non_empty_metrics():
+    gauge.set(1)  # Gauge is not supported yet
+    counter_no_labels.inc(1)
+    # counter_with_labels.labels("bar").inc(2)
+    # histogram_no_labels.observe(1)
+    # histogram_with_labels.labels("bar").observe(1)
+
+    metrics = get_metrics()
+
+    expected = """# HELP prometheus_spec_counter_no_labels_total Prometheus example counter without labels
+# TYPE prometheus_spec_counter_no_labels_total counter
+prometheus_spec_counter_no_labels_total 1.0
+"""
+
+    assert metrics == expected
