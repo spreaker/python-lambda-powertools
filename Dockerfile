1dd4afcd0e3f992c04061281d5517b46f781d272
Mauro Piccotti <mauro.piccotti@spreaker.com>
2025-05-02 15:20:05 +0200

    Dependencies update

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.9.16-alpine3.17
+FROM python:3.10.17-alpine3.20
 
 RUN  mkdir -p /workspace/python-lambda-powertools
 
--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import find_packages, setup
 setup(
     name='lambda_powertools',
     packages=find_packages(include=['lambda_powertools']),
-    version='0.2.0',
+    version='0.2.1',
     description='Lambda Powertools is a package encapsulating utilities and best practices used to write Python Lambda functions at Spreaker.',
     author='Spreaker',
     license='MIT',
-    install_requires=['prometheus-client==0.18.0'],
+    install_requires=['prometheus-client==0.21.1'],
     setup_requires=['pytest-runner'],
-    tests_require=['pytest==7.2.2', 'pytest-mock==3.10.0'],
+    tests_require=['pytest==8.3.5', 'pytest-mock==3.14.0'],
     test_suite='tests'
 )
